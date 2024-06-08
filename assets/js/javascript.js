90223ebd9c15391d782a4e8b150ee56fd19e79f1
Sergio Martinez <sergmart7@Sergios-MacBook-Air.local>
2024-06-07 19:19:07 -0600

    lightmode bug fix

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,20 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggleButton = document.getElementById('theme-toggle');
-    const currentTheme = localStorage.getItem('theme');
+//TOGGLE BUTTON FEATURE
 
-    if (currentTheme === 'light') {
-        document.body.classList.remove('dark-theme');
-    } else {
-        document.body.classList.add('dark-theme');
-    }
+// document.addEventListener('DOMContentLoaded', () => {
+//     const themeToggleButton = document.getElementById('theme-toggle');
+//     const currentTheme = localStorage.getItem('theme');
 
-    themeToggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-theme');
+//     if (currentTheme === 'light') {
+//         document.body.classList.remove('dark-theme');
+//     } else {
+//         document.body.classList.add('dark-theme');
+//     }
 
-        let theme = 'light';
-        if (document.body.classList.contains('dark-theme')) {
-            theme = 'dark';
-        }
-        localStorage.setItem('theme', theme);
-    });
-});
+//     themeToggleButton.addEventListener('click', () => {
+//         document.body.classList.toggle('dark-theme');
+
+//         let theme = 'light';
+//         if (document.body.classList.contains('dark-theme')) {
+//             theme = 'dark';
+//         }
+//         localStorage.setItem('theme', theme);
+//     });
+// });
