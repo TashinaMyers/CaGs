adc071c13bbea05eef73a686cae84c38edfad2a4
Sergio Martinez <sergmart7@Sergios-MacBook-Air.local>
2024-06-07 19:27:54 -0600

    commented out unused code

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,20 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggleButton = document.getElementById('theme-toggle' /*NAME SUBJECT TO CHANGE*/);
-    const currentTheme = localStorage.getItem('theme' /*NAME SUBJECT TO CHANGE*/);
+//TOGGLE BUTTON FEATURE
 
-    if (currentTheme === 'light') {
-        document.body.classList.remove('dark-theme' /*NAME SUBJECT TO CHANGE*/);
-    } else {
-        document.body.classList.add('dark-theme' /*NAME SUBJECT TO CHANGE*/);
-    }
+// document.addEventListener('DOMContentLoaded', () => {
+//     const themeToggleButton = document.getElementById('theme-toggle');
+//     const currentTheme = localStorage.getItem('theme');
 
-    themeToggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-theme' /*NAME SUBJECT TO CAHNGE*/);
+//     if (currentTheme === 'light') {
+//         document.body.classList.remove('dark-theme');
+//     } else {
+//         document.body.classList.add('dark-theme');
+//     }
 
-        let theme = 'light' /*NAME SUBJECT TO CHANGE*/;
-        if (document.body.classList.contains('dark-theme' /*NAME SUBJECT TO CHANGE*/)) {
-            theme = 'dark' /*NAME SUBJECT TO CHANGE*/;
-        }
-        localStorage.setItem('theme', theme /*NAME SUBJECT TO CHANGE*/);
-    });
-});
+//     themeToggleButton.addEventListener('click', () => {
+//         document.body.classList.toggle('dark-theme');
+
+//         let theme = 'light';
+//         if (document.body.classList.contains('dark-theme')) {
+//             theme = 'dark';
+//         }
+//         localStorage.setItem('theme', theme);
+//     });
+// });
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,22 +1,22 @@
 //TOGGLE BUTTON FEATURE
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const themeToggleButton = document.getElementById('theme-toggle');
-//     const currentTheme = localStorage.getItem('theme');
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
 
-//     if (currentTheme === 'light') {
-//         document.body.classList.remove('dark-theme');
-//     } else {
-//         document.body.classList.add('dark-theme');
-//     }
+    if (currentTheme === 'light') {
+        document.body.classList.remove('dark-theme');
+    } else {
+        document.body.classList.add('dark-theme');
+    }
 
-//     themeToggleButton.addEventListener('click', () => {
-//         document.body.classList.toggle('dark-theme');
+    themeToggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme');
 
-//         let theme = 'light';
-//         if (document.body.classList.contains('dark-theme')) {
-//             theme = 'dark';
-//         }
-//         localStorage.setItem('theme', theme);
-//     });
-// });
+        let theme = 'light';
+        if (document.body.classList.contains('dark-theme')) {
+            theme = 'dark';
+        }
+        localStorage.setItem('theme', theme);
+    });
+});
