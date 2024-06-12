6ada7e1e3e9d1d41f713b8a055df8fe85389cb90
Jocephuss <rowlandjosiah14@gmail.com>
2024-06-11 22:23:05 -0400

    Changed button hover

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,31 +1,31 @@
 // FUNCTION TO HANDLE OPENING AND CLOSING OF MODALS
 
 const initializeModals = () => {
-  const images = document.querySelectorAll('#myImg');
-  const modals = document.querySelectorAll('.modal');
+    const images = document.querySelectorAll('#myImg');
+    const modals = document.querySelectorAll('.modal');
 
-  images.forEach((img, index) => {
-      const modal = modals[index];
-      const modalImg = modal.querySelector('.modal-content');
-      const captionText = modal.querySelector('.caption');
-      const closeBtn = modal.querySelector('.close');
+    images.forEach((img, index) => {
+        const modal = modals[index];
+        const modalImg = modal.querySelector('.modal-content');
+        const captionText = modal.querySelector('.caption');
+        const closeBtn = modal.querySelector('.close');
 
-      img.addEventListener('click', () => {
-          modal.style.display = 'block';
-          modalImg.src = img.src;
-          captionText.innerHTML = img.alt;
-      });
+        img.addEventListener('click', () => {
+            modal.style.display = 'block';
+            modalImg.src = img.src;
+            captionText.innerHTML = img.alt;
+        });
 
-      closeBtn.addEventListener('click', () => {
-          modal.style.display = 'none';
-      });
+        closeBtn.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
 
-      modal.addEventListener('click', (event) => {
-          if (event.target === modal) {
-              modal.style.display = 'none';
-          }
-      });
-  });
+        modal.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    });
 }
 
 // INITIALIZES MODALS FUNCTION UPON LOADING IN OUR PAGE
